7fb4a69fdd2f2a830f4d00a038ab9f9b66b58521
Rich T <richard.todd@shopify.com>
2020-11-12 13:27:59 -0500

    Finished Chapter 5

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,10 @@
+# typed: false
+# frozen_string_literal: true
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+end
