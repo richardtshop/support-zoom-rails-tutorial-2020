22d0f544135d5b16612455f7f243e34dba337ed5
Rich T <richard.todd@shopify.com>
2020-11-16 15:31:50 -0500

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+# typed: false
+# frozen_string_literal: true
+class User < ApplicationRecord
+  validates(:name, presence: true, length: { maximum: 50 })
+  validates(:email, presence: true, length: { maximum: 255 })
+end
--- example_user.rb
+++ example_user.rb
@@ -1,24 +1,14 @@
 # typed: false
 # frozen_string_literal: true
 class User
-  attr_accessor :first_name, :last_name, :email
+  attr_accessor :name, :email
 
   def initialize(attributes = {})
-    @first_name = attributes[:first_name]
-    @last_name = attributes[:last_name]
-
+    @name = attributes[:first_name]
     @email = attributes[:email]
   end
 
   def formatted_email
-    "#{full_name} <#{@email}>"
-  end
-
-  def full_name
-    "#{@first_name} #{@last_name}"
-  end
-
-  def alphabetical_name
-    "#{@last_name}, #{@first_name}"
+    "#{name} <#{@email}>"
   end
 end
