c396a910d4de5135d4f0b0076ded0c293d5b19c4
Rich T <richard.todd@shopify.com>
2020-11-20 11:08:55 -0500

    Fixed parenthesis issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to(@user) # Same as redirect_to(user_url(@user))
+      redirect_to @user # Same as redirect_to user_url(@user)
     else
-      render('new')
+      render 'new'
     end
   end
 
